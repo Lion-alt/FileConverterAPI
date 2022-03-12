bbd7bd8e0bdd7a43235d48842afe68d0a6d2c7e3
zdebil <zdebil@bbcag.ch>
2022-03-12 11:57:50 +0100

    added error handling

--- api/src/htmlToPdf/htmlToPdf.ts
+++ api/src/htmlToPdf/htmlToPdf.ts
@@ -3,9 +3,6 @@ import type { ReadStream as ReadStreamType } from "fs";
 import { create } from "html-pdf";
 import { configureOptionsForPdf } from "./configureOptonsForPdf.js";
 /**
- * Activity diagram
- * http://www.plantuml.com/plantuml/uml/JOuzQWCn48NxFSNKov83n8r9boQOGEemNZr52hDQCHhPEAUFf43iHeZtyxqv5cErkWbz8K7Py95Mg4KVuKxEbsa4fcTwHnPUGGjvtJVoKP1u0rsZ1VeyjCIfba0My8zob9rl2Xl5yg0EGB0soidrng2zTLfCjZULM9LlnAN_z_CtLkiBhvvOX5wgzz3cwSFMCa3rVlsm2_h2FhnL8f9rw4_CnDwWNRzHu0le3BJPd5i1ey8zjj0rG37wZesfgfG_ikqAzpGLNA0iDCyI4yey7wQ6c8xjgPly0W00
- *
  * @param {Array<string>} fileNames array of fileNames from pdfs.
  * @param {Array<string>} htmlTemplateArray array of html-strings, that are converted to pdfs.
  * @param {CreateOptionType} htmlToPDfOptions  options for converting html to pdf.
@@ -17,7 +14,9 @@ export const generatePdfFromHtml: Function = async (
 ): Promise<void> => {
   create(html, configureOptionsForPdf()).toStream(
     (err: unknown, result: ReadStreamType) => {
-      console.log(result)
+      if(err) {
+        res.status(500).send(err);
+      }
       result.pipe(res);
     }
   );
